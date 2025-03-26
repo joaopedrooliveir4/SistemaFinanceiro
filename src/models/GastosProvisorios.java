1c589c32b1f368f73a9149c274cc9b72a7a32776
joaopedrooliveir4 <jp3066984@gmail.com>
2025-03-26 17:04:45 -0300

    Classe GastosProvisorios herda da classe Gastos e reprsenta um tipo de Gasto mensal

---
+++ src/models/GastosProvisorios.java
@@ -0,0 +1,7 @@
+package models;
+
+public class GastosProvisorios extends Gastos {
+    public GastosProvisorios(String descricao, double valor, String tipo, String categoria) {
+        super(descricao, valor, tipo, categoria);
+    }
+}
